cac762543d6c73b2e56b1ba7a4a421df60bf34a4
cool22lucky@gmail.com <cool22lucky@gmail.com>
2017-07-23 00:38:08 +0530

    About page update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,8 @@ import { AppRoutingModule, routedComponents } from '../app-routing.module';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'about.component.html'
+    templateUrl: 'about.component.html',
+    styleUrls: ['./about.component.css']
 })
 
 export class AboutComponent {

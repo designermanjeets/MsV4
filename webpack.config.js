e6a481ea0a2a9fee0d62b85b41f067318cf836b4
cool22lucky@gmail.com <cool22lucky@gmail.com>
2017-08-03 23:41:42 +0530

    webpack trying to resolve

--- webpack.config.js
+++ webpack.config.js
@@ -396,7 +396,7 @@ module.exports = {
     }),
     new ProgressPlugin(),
     new HtmlWebpackPlugin({
-      "template": "./src\\index.html",
+      "template": "./src/index.html",
       "filename": "./index.html",
       "hash": false,
       "inject": true,
